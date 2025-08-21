8d672475128d1080607c0266548d055036bd5bc3
myogin <yoginugraha19@gmail.com>
2025-08-21 19:15:15 +0800

    update default route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 // routes/web.php
 Route::view('/test-vite', 'test-vite');
